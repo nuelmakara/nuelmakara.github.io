266f52b928169ce535fe6facd907588362c3874d
NuelMakara <nuelmakara@gmail.com>
2018-07-01 13:13:07 -0400

    Style and Documentation update to designs.js
    Added descriptions to the styling and updated the ID tags to classes which are better and more descriptive.
    explained the various elements, the functions and methods that combined to make the web App run smoothly!
    
    Trying to meet the Style Guide requirements!

--- designs.js
+++ designs.js
@@ -1,31 +1,60 @@
-// Select color input
-function invokeColor() {
-    let col = $("td");
-    col.click(function () {
-    const color = $("#colorPicker").val();
+// .colorPicker; .pixelCanvas; .sizePicker; .inputHeight and .inputWidth represents the HTML Elements of (ColorPallette)+
+//Table, table rows and columns  sizes, and input field for capturing sizes of the rows and columns!
+/**
+* @description Targets the colorPicker (.colorPicker) 
+*which sets the value selected by the user to 'col' *variable corresponds to a '<td>'.
+*The '<td>' has an event listener that attaches the *selected colorPicker value to the <td> element when *clicked! on('click');
+*The Css style gets applied to the <td> onClick event *and on doubleClick removes (.removeAttr()) from the *style attribute from the <td> element.
+*/
+function invokeColor(){
+  let col = $("td");
+    col.click(function(){
+    const color = $(".colorPicker").val();
     if ($(this).attr("style")){
-        $(this).removeAttr("style");
-     } else {
-        $(this).attr("style", "background-color:" + color);
+   $(this).removeAttr("style");
+ } 
+   else {
+    $(this).attr("style","background-color:"+ color);
      }
-    }
-   );
+  });
 }
-// Select size input
-$("#sizePicker").submit(function(evt){
-    evt.preventDefault();
-    let height = $("#inputHeight").val();
-    let width = $("#inputWidth").val();
-   makeGrid(height, width);
-    invokeColor();
+/**
+* @description Targets the table element(.sizePicker) *and uses a submit function with a corresponding *'.preventdefault()':[to prevent the form from *submitting] method to take the values of grid height *and grid width and draw <td> and <tr>.
+*setting the values as parameters for makeGrid();
+*event listener handles the firing of the submit action *and calls another method evt.preventDefault()
+* @param {variable} height which gets values *from .inputHeight.
+* @param {variable} width which gets its value *from .inputWidth.
+*@description calls the invokeColor function which *applies the chosen color to the cell <td> selected by *the user.
+*/
+// Select size input and output a grid of same dimension
+$(".sizePicker").submit(function(evt){
+  evt.preventDefault();
+    let height = $(".inputHeight").val();
+      let width = $(".inputWidth").val();
+    makeGrid(height, width);
+  invokeColor();
 });
-
-// Your code goes here!
-function makeGrid(row,col){$("tr").remove();
+/**
+* @description Takes two parameters representing 
+*'row' and 'column' and uses the return values to draw *corresponding <td> and <tr> elements on the PixelCanvas *(table)
+* @param {variable} row
+* @param {variable} col
+* @returns {grid} a table with appended '<tr></tr>' that *gets appended to <td></td>.
+*@deacription A while loop is used to loop through the *different values of .inputHeight and .inputWidth so *that a corresponding grid of rows and cols can be *drawn.
+*/
+function makeGrid(row,col){
+  $("tr").remove();
   let i=1;
-  while(i <= row){$("#pixelCanvas").append("<tr></tr>");i++;}
-   let j=1;
-  while(j <= col){$("tr").append("<td></td>");j++;}
+    while(i <= row){
+    $(".pixelCanvas").append("<tr></tr>");
+  i++;
 }
-// add colors to the cells
+  let j=1;
+    while(j <= col){
+    $("tr").append("<td></td>");
+  j++;
+}
+}
+
+
 
